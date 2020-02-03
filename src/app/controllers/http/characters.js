e7d7df779dc69962b043262a095e39a0e4ef7551
Douglas Haubert <dhaubert.ti@gmail.com>
2020-02-03 11:41:19 -0300

    get characters: searching by name

--- src/app/controllers/http/characters.js
+++ src/app/controllers/http/characters.js
@@ -1,4 +1,5 @@
 const { Character, Comic } = require("../../models");
+const Sequelize = require('sequelize');
 
 class CharacterController {
   constructor() {
@@ -7,12 +8,18 @@ class CharacterController {
   async index(request, response) {
     try {
       const { offset, limit } = request.params;
-      
+      const { search } = request.query;
+
       const result = await Character.findAll({
-        attributes: ['id', 'name', 'description', 'thumbnail'],
+        attributes: ["id", "name", "description", "thumbnail"],
         include: {
           model: Comic,
-          through: {attributes: []}
+          through: { attributes: [] }
+        },
+        where: {
+          name: {
+            [Sequelize.Op.like]: '%'+ search +'%'
+          }
         },
         offset,
         limit,
@@ -30,13 +37,19 @@ class CharacterController {
       const { id } = request.params;
       const result = await Character.findByPk(id);
       if (!result) {
-          throw { errorCode: 404, message: `Character ${id} not found`, error: true};
+        throw {
+          errorCode: 404,
+          message: `Character ${id} not found`,
+          error: true
+        };
       }
       response.json(result);
     } catch (error) {
-      return response
-        .status(error.errorCode)
-        .json({ error: true, errorCode: error.errorCode, errorMessage: error.message });
+      return response.status(error.errorCode).json({
+        error: true,
+        errorCode: error.errorCode,
+        errorMessage: error.message
+      });
     }
   }
 
@@ -46,18 +59,25 @@ class CharacterController {
       const result = await Character.findByPk(id, {
         include: {
           model: Comic,
-          through: {attributes: []}
+          through: { attributes: [] }
         }
       });
-      const noComicsFound = !result || !result.Comics || result.Comics.length === 0;
+      const noComicsFound =
+        !result || !result.Comics || result.Comics.length === 0;
       if (noComicsFound) {
-          throw { errorCode: 404, message: `No comics found for ${id}`, error: true};
+        throw {
+          errorCode: 404,
+          message: `No comics found for ${id}`,
+          error: true
+        };
       }
       response.json(result.Comics);
     } catch (error) {
-      return response
-        .status(error.errorCode || 400)
-        .json({ error: true, errorCode: error.errorCode, errorMessage: error.message });
+      return response.status(error.errorCode || 400).json({
+        error: true,
+        errorCode: error.errorCode,
+        errorMessage: error.message
+      });
     }
   }
 }
